6b125b12c85f190427b0f79872c7f262697b65db
sabeel k m <sabeelmuttil@gmail.com>
2021-03-30 18:31:20 +0530

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import ResponsiveFonts from './responsive-fonts'
+import { Breakpoint } from './make-responsive-component'
+import {
+  MediumAndUp,
+  LargeAndUp,
+  SmallAndDown,
+  MediumAndDown,
+  MediumOnly,
+} from './breakpoints.jsx'
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+  text-align: center;
+`
+
+export default class extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <ResponsiveFonts>
+          <SmallAndDown>sm and down</SmallAndDown>
+          <MediumAndUp>med and up</MediumAndUp>
+          <MediumAndDown>med and down</MediumAndDown>
+          <MediumOnly>medium only</MediumOnly>
+          <LargeAndUp>lg and up</LargeAndUp>
+          <Breakpoint max="1200px">only on huge width</Breakpoint>
+        </ResponsiveFonts>
+      </Wrapper>
+    )
+  }
+}
